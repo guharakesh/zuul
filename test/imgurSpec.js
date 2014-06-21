9c1c98554789070fb18b7ce65a243bb69377a535
vagrant <vagrant@precise64.(none)>
2014-06-21 00:38:06 +0000

    start writing test

---
+++ test/imgurSpec.js
@@ -0,0 +1,18 @@
+var expect = require("chai").expect;
+var imgur = require("../lib/imgur.js");
+
+describe("Imgur", function() {
+	describe("#upload()", function() {
+		it("should return an imgur link with the uploaded image", function(done) {
+			this.timeout(50000);
+			var url = 'http://www.clevescene.com/imager/the-lights-of-downtown-cleveland-reflected/b/slideshow/4326741/f5c7/s-1.jpg';
+			
+			imgur.upload(url,function(results) {
+				expect(results).to.match(/^http:\/\/imgur\.com\/[a-zA-Z0-9]{7}$/);
+				// expect(results).to.be.a('string');
+				done();
+			});
+
+			});
+	});	
+});
