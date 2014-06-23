ca0da8356a51669317a653f163a12d8e81cb8521
Rakesh Guha <guha.rakesh@gmail.com>
2014-06-23 23:57:23 +0000

    upload images

--- test/imgurSpec.js
+++ test/imgurSpec.js
@@ -8,9 +8,24 @@ describe("Imgur", function() {
 			var url = 'http://www.clevescene.com/imager/the-lights-of-downtown-cleveland-reflected/b/slideshow/4326741/f5c7/s-1.jpg';
 			
 			imgur.upload(url,function(results) {
-				expect(results).to.match(/^http:\/\/i\.imgur\.com\/[a-zA-Z0-9]{7}\.jpg$/);
+				expect(results).to.match(/^[a-zA-Z0-9]{7}$/);
 				done();
 			});
 		});
 	});	
+
+	describe("#upImages()", function() {
+		it("should return an array of uploaded image ids", function(done) {
+			this.timeout(200000);
+			var url = 'http://www.clevescene.com/cleveland/12-cleveland-patios-you-have-to-hit-this-summer/Slideshow?oid=4328959&slide=';
+			imgur.upImages(url, function(results){
+				expect(results).to.be.instanceOf(Array);
+				expect(results).to.not.be.empty;
+				for (var i=0; i<results.length; i++) {
+					expect(results[i]).to.match(/^[a-zA-Z0-9]{7}$/);
+				}
+				done();	
+			});
+		});
+	});
 });
--- test/sceneSpec.js
+++ test/sceneSpec.js
@@ -2,7 +2,7 @@ var expect = require("chai").expect;
 var scene = require("../lib/scene.js");
 
 describe("Scene", function() {
-	describe("getJPG()", function() {
+	describe("#getJPG()", function() {
 		it("should return a url to the jpg on the page", function(done) {
 			var url = 'http://www.clevescene.com/cleveland/10-greater-cleveland-all-day-breakfast-joints-you-should-try/Slideshow?oid=4317427&slide=1';
 
@@ -13,13 +13,12 @@ describe("Scene", function() {
 		});
 	});
 
-	describe("slideURLs", function() {
+	describe("#slideURLs()", function() {
 		it("should return a base url w/o slide #, and # of slides", function(done) {
 			this.timeout(50000);
 			var url = ['http://www.clevescene.com/scene-and-heard/archives/2014/05/30/10-greater-cleveland-all-day-breakfast-joints-you-should-try', 'http://www.clevescene.com/cleveland/12-cleveland-patios-you-have-to-hit-this-summer/Slideshow?oid=4328959'];
-			var testFunction = function(base_url, count) {
+			var testFunction = function(base_url) {
 				expect(base_url).to.match(/^http:\/\/www.clevescene\.com\/cleveland\/[a-zA-Z0-9\-]+\/Slideshow\?oid=[0-9]{7}&slide=$/);
-				expect(count).to.be.a('number');
 			};
 			for (var i=0; i<url.length; i++) {
 				scene.baseURL(url[i], testFunction);
